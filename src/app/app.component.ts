d861e61936f0942bb0351a5731e8aae517c7ec8b
Alex Vladimirov <alexey.vladimirov@team.wrike.com>
2023-11-14 19:20:55 +0100

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, inject } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'ng17-ssr';
+  test = inject('TEST' as any);
+
+  ngOnInit(): void {
+      console.log('Test', this.test)
+  }
+}
